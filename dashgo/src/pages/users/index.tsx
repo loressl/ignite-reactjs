eaaa4466b464958e97ec004dcd9b5ff9503bfdf9
loressl <loryssl@hotmail.com>
2022-05-06 22:09:45 -0300

    Chapter IV-Construindo a aplicação

--- dashgo/src/pages/users/index.tsx
+++ dashgo/src/pages/users/index.tsx
@@ -13,44 +13,36 @@ import {
     Tr,
     Text,
     useBreakpointValue,
-    Spinner
+    Spinner,
+    Link
 } from "@chakra-ui/react";
-import Link from "next/link";
-import { useEffect } from "react";
+import NextLink from "next/link";
+import { useState } from "react";
 import { RiAddLine } from "react-icons/ri";
-import { useQuery } from 'react-query'
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { api } from "../../services/api";
+import { useUsers } from "../../services/hooks/useUsers";
+import { queryClient } from "../../services/queryClient";
 
 export default function UserList() {
-
-    const { data, isLoading, error } = useQuery('users', async () => {
-        const response = await fetch('http://localhost:3000/api/users')
-        const data = await response.json()
-
-        const users = data.users.map(user => {
-            return {
-                id: user.id,
-                name: user.name,
-                email: user.email,
-                createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-                    day: '2-digit',
-                    month: 'long',
-                    year: 'numeric'
-                })
-            }
-        })
-
-        return users
-    },{
-        staleTime: 1000 * 5, // 5 segundos
-    })
+    const [page, setPage] = useState(1)
+    const { data, isLoading, isFetching, error } = useUsers(page)
 
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true
     })
+
+    async function handlePrefetchUser(userId: string){
+        await queryClient.prefetchQuery(['user', userId], async () => {
+            const response = await api.get(`users/${userId}`)
+            return response.data
+        }, {
+            staleTime: 1000 * 60 * 10, // 10 min que os dados ficaram fresh
+        })
+    }
 
     return(
         <Box>
@@ -59,9 +51,12 @@ export default function UserList() {
                 <Sidebar />
                 <Box flex="1" borderRadius={8} bg="gray.800" p="8">
                     <Flex mb="8" justify="space-between" align="center">
-                        <Heading size="lg" fontWeight="normal">Usuários</Heading>
+                        <Heading size="lg" fontWeight="normal">
+                            Usuários
+                            {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />}
+                        </Heading>
 
-                        <Link href="/users/create" passHref>
+                        <NextLink href="/users/create" passHref>
                             <Button 
                                 as="a" 
                                 size="sm" 
@@ -71,7 +66,7 @@ export default function UserList() {
                             >
                                 Criar novo
                             </Button>
-                        </Link>
+                        </NextLink>
                     </Flex>
                     {isLoading ? (
                         <Flex justify="center">
@@ -95,7 +90,7 @@ export default function UserList() {
                                     </Tr>
                                 </Thead>
                                 <Tbody>
-                                    {data.map(user => {
+                                    {data.users.map(user => {
                                         return (
                                         <Tr key={user.id}>
                                             <Td px={["4","4","6"]}>
@@ -103,7 +98,9 @@ export default function UserList() {
                                             </Td>
                                             <Td>
                                                 <Box>
-                                                    <Text fontWeight="bold">{user.name}</Text>
+                                                    <Link color="purple.400" onMouseEnter={() => handlePrefetchUser(user.id)}>
+                                                        <Text fontWeight="bold">{user.name}</Text>
+                                                    </Link>
                                                     <Text fontSize="sm" color="gray.300">{user.email}</Text>
                                                 </Box>
                                             </Td>
@@ -125,7 +122,11 @@ export default function UserList() {
                                 </Tbody>
                             </Table>
 
-                            <Pagination />
+                            <Pagination 
+                                totalCountOfRegisters={data.totalCount}
+                                currentPage={page}
+                                onPageChange={setPage}
+                            />
                         </>
                     )}
                 </Box>
