343aa8392cc379ee59ebd5887b1808fce65b4679
loressl <loryssl@hotmail.com>
2022-05-16 21:33:49 -0300

    Chapter IV- Autenticação com JWT

--- authentication/services/api.ts
+++ authentication/services/api.ts
@@ -1,11 +1,74 @@
-import axios from 'axios'
-import { parseCookies } from 'nookies'
+import axios, { AxiosError } from 'axios'
+import { parseCookies, setCookie } from 'nookies'
+import { signOut } from '../context/AuthContext'
 
-const cookies = parseCookies()
+let cookies = parseCookies()
+let isRefreshing = false
+let failedRequestsQueue = []
 
 export const api = axios.create({
     baseURL: 'http://localhost:3333',
     headers: {
         Authorization: `Bearer ${cookies['nextauth.token']}`
     }
+})
+
+api.interceptors.response.use(response => {
+    return response
+}, (error: AxiosError) => {
+    if(error.response.status === 401) {
+        if(error.response.data['code'] === 'token.expired'){
+            cookies = parseCookies()
+
+            const { 'nextauth.refreshToken': refreshToken } = cookies
+            const originalConfig = error.config
+
+            if(!isRefreshing){
+                isRefreshing= true
+
+                api.post('/refresh', {
+                    refreshToken,
+                }).then(response => {
+                    const {token} = response.data
+                    setCookie(undefined, 'nextauth.token', token, {
+                        maxAge: 60*60*24*30, // 30 days
+                        path: '/' // o / indica que qualquer endereço da aplicação terá acesso
+                    })
+        
+                    setCookie(undefined, 'nextauth.refreshToken', response.data.refreshToken, {
+                        maxAge: 60*60*24*30, // 30 days
+                        path: '/' // o / indica que qualquer endereço da aplicação terá acesso
+                    })
+                    api.defaults.headers['Authorization'] = `Bearer ${token}`
+
+                    failedRequestsQueue.forEach(request => request.onSuccess(token))
+                    failedRequestsQueue = []
+                }).catch(err => {
+                    failedRequestsQueue.forEach(request => request.onFailure(err))
+                    failedRequestsQueue = []
+                }).finally(() => {
+                    isRefreshing = false
+                })
+            } 
+
+            // resolve e reject aguarda as coisas a serem executadas
+            return new Promise((resolve, reject) => {
+                failedRequestsQueue.push({
+                    onSuccess:(token:string)=>{
+                        originalConfig.headers['Authorization'] = `Bearer ${token}`
+
+                        resolve(api(originalConfig))
+                    },
+                    onFailure:(err: AxiosError)=>{
+                        reject(err)
+                    },
+                })
+            })
+        } else {
+            signOut()
+
+        }
+    }
+
+    return Promise.reject(error)
 })
