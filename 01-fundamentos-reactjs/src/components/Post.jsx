4f787764f22247c7411b36b610d283827df58eda
loressl <loryssl@hotmail.com>
2022-07-07 21:46:58 -0300

    feat: Inserindo comentários

--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -7,7 +7,8 @@ import styles from './Post.module.css'
 
 
 export function Post({author, publishedAt, content}){
-    const [comments, setComments]= useState([1, 2])
+    const [comments, setComments]= useState(['Post muito bacana, heim?'])
+    const [newCommentText, setNewCommentText] = useState('')
 
     const publishedDateFormated = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
         locale: ptBR,
@@ -20,7 +21,13 @@ export function Post({author, publishedAt, content}){
 
     function handleCreateNewComment() {
         event.preventDefault()
-        setComments([...comments, comments.length + 1])
+
+        setComments([...comments, newCommentText])
+        setNewCommentText('')
+    }
+
+    function handleNewCommentChange() {
+        setNewCommentText(event.target.value)
     }
 
     return(
@@ -69,7 +76,10 @@ export function Post({author, publishedAt, content}){
                 <strong>Deixe seu feedback</strong>
 
                 <textarea
+                    name='comment'
                     placeholder='Deixe um comentário'
+                    value={newCommentText}
+                    onChange={handleNewCommentChange}
                 >
 
                 </textarea>
@@ -83,7 +93,7 @@ export function Post({author, publishedAt, content}){
 
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment />
+                    return <Comment content={comment} />
                 })}
             </div>
         </article>
