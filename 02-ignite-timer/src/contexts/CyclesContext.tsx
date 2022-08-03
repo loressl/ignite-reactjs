aeb913d90016d5711cef3372539a082c0ae7c8a1
loressl <loryssl@hotmail.com>
2022-08-02 22:00:34 -0300

    chore: Troca useState por useReducer

--- 02-ignite-timer/src/contexts/CyclesContext.tsx
+++ 02-ignite-timer/src/contexts/CyclesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useState } from "react"
+import { createContext, ReactNode, useReducer, useState } from "react"
 
 interface CreateCycleData {
     task: string
@@ -31,10 +31,55 @@ interface CyclesContextProviderPros {
     children: ReactNode
 }
 
+interface CyclesState {
+    cycles: Cycle[]
+    activeCycleId: string | null
+}
+
 export function CyclesContextProvider({ children }: CyclesContextProviderPros) {
-    const [cycles, setCycles] = useState<Cycle[]>([])
-    const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+    const [cyclesState, dispatch] = useReducer((state: CyclesState, action: any) => {
+        switch (action.type) {
+            case 'ADD_NEW_CYCLE':
+                return {
+                    ...state,
+                    cycles: [...state.cycles, action.payload.newCycle],
+                    activeCycleId: action.payload.newCycle.id
+                }
+            case 'INTERRUPT_CURRENT_CYCLE':
+                return {
+                    ...state,
+                    cycles: state.cycles.map(cycle => {
+                        if (cycle.id === state.activeCycleId) {
+                            return { ...cycle, interrupedDate: new Date() }
+                        } else {
+                            return cycle
+                        }
+                    }),
+                    activeCycleId: null,
+                }
+            case 'MARK_CURRENT_CYCLE_AS_FINISHED':
+                return {
+                    ...state,
+                    cycles: state.cycles.map(cycle => {
+                        if (cycle.id === state.activeCycleId) {
+                            return { ...cycle, finishedDate: new Date() }
+                        } else {
+                            return cycle
+                        }
+                    }),
+                    activeCycleId: null,
+                }
+            default:
+                return state
+        }
+    }, {
+        cycles: [],
+        activeCycleId: null,
+    })
+
     const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+
+    const { cycles, activeCycleId } = cyclesState
 
     const activeCycle = cycles.find(cycle => cycle.id === activeCycleId)
 
@@ -43,13 +88,12 @@ export function CyclesContextProvider({ children }: CyclesContextProviderPros) {
     }
 
     function markCurrentCycleAsFinished() {
-        setCycles((state) => state.map(cycle => {
-            if (cycle.id === activeCycleId) {
-                return { ...cycle, finishedDate: new Date() }
-            } else {
-                return cycle
+        dispatch({
+            type: 'MARK_CURRENT_CYCLE_AS_FINISHED',
+            payload: {
+                activeCycleId
             }
-        }))
+        })
     }
 
     function createNewCycle(data: CreateCycleData) {
@@ -60,22 +104,22 @@ export function CyclesContextProvider({ children }: CyclesContextProviderPros) {
             minutesAmount: data.minutesAmount,
             startDate: new Date(),
         }
-        setCycles((state) => [...state, newCycle])
-        setActiveCycleId(id)
+        dispatch({
+            type: 'ADD_NEW_CYCLE',
+            payload: {
+                newCycle
+            }
+        })
         setAmountSecondsPassed(0)
-
-        //reset()
     }
 
     function interruptCurrentCycle() {
-        setCycles((state) => state.map(cycle => {
-            if (cycle.id === activeCycleId) {
-                return { ...cycle, interrupedDate: new Date() }
-            } else {
-                return cycle
+        dispatch({
+            type: 'INTERRUPT_CURRENT_CYCLE',
+            payload: {
+                activeCycleId
             }
-        }))
-        setActiveCycleId(null)
+        })
         document.title = 'Ignite Timer'
     }
 
--- 02-ignite-timer/src/pages/History/index.tsx
+++ 02-ignite-timer/src/pages/History/index.tsx
@@ -1,11 +1,13 @@
 import { useContext } from "react";
+import { formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 import { CyclesContext } from "../../contexts/CyclesContext";
 import { HistoryContainer, HistoryList, Status } from "./styles";
 
-export function History(){
-    const {cycles} = useContext(CyclesContext)
+export function History() {
+    const { cycles } = useContext(CyclesContext)
 
-    return(
+    return (
         <HistoryContainer>
             <h1>Meu Histórico</h1>
 
@@ -20,30 +22,23 @@ export function History(){
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20min</td>
-                            <td>Há 3 meses</td>
-                            <td>
-                                <Status statusColor="green">Concluído</Status>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20min</td>
-                            <td>Há 3 meses</td>
-                            <td>
-                                <Status statusColor="green">Concluído</Status>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20min</td>
-                            <td>Há 3 meses</td>
-                            <td>
-                                <Status statusColor="green">Concluído</Status>
-                            </td>
-                        </tr>
+                        {cycles.map(cycle => {
+                            return (
+                                <tr key={cycle.id}>
+                                    <td>{cycle.task}</td>
+                                    <td>{cycle.minutesAmount} minutos</td>
+                                    <td>{formatDistanceToNow(cycle.startDate, {
+                                        addSuffix: true,
+                                        locale: ptBR,
+                                    })}</td>
+                                    <td>
+                                        {cycle.finishedDate && <Status statusColor="green">Concluído</Status>}
+                                        {cycle.interrupedDate && <Status statusColor="red">Interrompido</Status>}
+                                        {(!cycle.finishedDate && !cycle.interrupedDate) && <Status statusColor="yellow">Em andamento</Status>}
+                                    </td>
+                                </tr>
+                            )
+                        })}
                     </tbody>
                 </table>
             </HistoryList>
